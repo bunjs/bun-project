ad928ace929bc03d393f698df639d40c0f3bbc9c
bunko <2leg@immaster.cn>
2019-08-07 21:39:22 +0800

    修复project命名写死的问题

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
 module.exports = {
     apps: [{
-        name: "protest",
+        name: "$_projectname",
         script: "./entry.js",
         watch: false,
+        kill_timeout: 10 * 1000,
         error_file: './logs/bun/bun.log.wf',
         out_file: './logs/bun/bun.log',
+        time: true,
         env: {
             "NODE_ENV": "development",
         },
